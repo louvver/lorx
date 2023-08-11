976af8a27a3be6bfd5cc083e7e60dbd2187ffd46
Mrcys <kalinigga@outlook.es>
2023-08-10 22:12:53 -0300

    fix: expires date

--- src/lib/utils/Services.ts
+++ src/lib/utils/Services.ts
@@ -47,10 +47,10 @@ export const expireDate = async () => {
 	const collection = db.collection('users');
 	const date = new Date();
 	const users = await collection.find().toArray();
-	const customers = users.filter(({ subscription }) => subscription?.expire != Infinity);
+	const customers = users.filter(({ subscription }) => subscription?.expires != Infinity);
 	if (!customers || !customers.length) return;
 	customers.forEach(async ({ id, username, subscription }) => {
-		if (date.getTime() === subscription.expire || new Date(subscription.expire) < date) {
+		if (date.getTime() === subscription.expires || new Date(subscription.expires) < date) {
 			await collection.findOneAndUpdate({ id }, {
 				$unset: {
 					subscription
